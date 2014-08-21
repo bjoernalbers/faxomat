1054893a152c7f69096762fcd8712fa3e61595a8
Björn Albers <bjoernalbers@gmail.com>
2014-08-22 00:21:47 +0200

    Preserve production database between deploys.

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :log_level, :info
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml db/production.sqlite3}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
