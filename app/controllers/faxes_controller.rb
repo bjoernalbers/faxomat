79394075971eda2e225d680f615cdda7a7f1ab17
Björn Albers <bjoernalbers@gmail.com>
2014-08-21 11:54:49 +0200

    Cleanup faxes controller.

--- app/controllers/faxes_controller.rb
+++ app/controllers/faxes_controller.rb
@@ -14,11 +14,6 @@ class FaxesController < ApplicationController
 
   def create
     # See http://stackoverflow.com/questions/9758879/sending-files-to-a-rails-json-api !!!
-
-    # Check if the file was uploaded via JSON (otherwise we'd get some multipart form).
-    # NOTE: Inside this block we can't use `fax_params` since the attributes
-    # from a multipart form are a bit different than our little JSON request.
-    #if params[:fax][:document][:data]
     if params[:fax][:document]
       tempfile = Tempfile.new('fileupload')
       tempfile.binmode
@@ -30,20 +25,6 @@ class FaxesController < ApplicationController
         type:     params[:fax][:document][:type])
     end
 
-    #if fax_params[:document]
-      #tempfile = Tempfile.new('fileupload')
-      #tempfile.binmode
-      #tempfile.write(Base64.decode64(fax_params[:document][:data]))
-#
-      #fax_params[:document] = ActionDispatch::Http::UploadedFile.new(
-        #tempfile: tempfile,
-        #filename: fax_params[:document][:filename],
-        #type:     fax_params[:document][:type])
-    #end
-    # start hacking
-    
-    # stop hacking
-    #fax = Fax.new(params[:fax])
     fax = Fax.new(fax_params)
 
     if fax.save
@@ -70,9 +51,7 @@ class FaxesController < ApplicationController
   private
 
   def fax_params
-    #params.require(:fax).permit(:path, :phone)
     params.require(:fax).permit(:title, :phone, :document)
-    #params.require(:fax).permit(:title, :phone, document: [:filename, :type, :data])
   end
 
   def faxes
