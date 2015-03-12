eed4f44243e6d8ccc7fcd0dd237a267eb7ad5df6
Björn Albers <bjoernalbers@gmail.com>
2015-03-12 09:15:25 +0100

    Data migration: create print jobs from faxes.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150311101210) do
+ActiveRecord::Schema.define(version: 20150312064853) do
 
   create_table "faxes", force: true do |t|
     t.integer  "recipient_id",                      null: false
