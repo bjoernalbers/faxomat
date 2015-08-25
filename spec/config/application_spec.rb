aa5d694281c859647805b972f98000461a8fe38a
Björn Albers <bjoernalbers@gmail.com>
2015-08-25 15:39:22 +0200

    Drop obsolete config spec.

--- spec/config/application_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Rails.application.config do
-  describe '.dialout_prefix' do
-    it 'is 0 by default' do
-      expect(Rails.application.config.dialout_prefix).to eq 0
-    end
-  end
-end
