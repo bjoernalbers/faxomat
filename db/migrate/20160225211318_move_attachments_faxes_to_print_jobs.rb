fb1a8f90eea8950be7710fb220fa9b2b9e7ecc4a
Björn Albers <bjoernalbers@gmail.com>
2016-02-25 22:33:04 +0100

    Rename attachments path from faxes to print_jobs.

---
+++ db/migrate/20160225211318_move_attachments_faxes_to_print_jobs.rb
@@ -0,0 +1,25 @@
+class MoveAttachmentsFaxesToPrintJobs < ActiveRecord::Migration
+  def up
+    if File.directory?(destination)
+      FileUtils.mv(Dir.glob("#{source}/*"), destination) && FileUtils.rmdir(source)
+    else
+      FileUtils.mv source, destination
+    end
+  end
+
+  def down
+    FileUtils.mv(destination, source)
+  end
+
+  def attachments_path
+    @attachments_path ||= Rails.root.join('storage', Rails.env)
+  end
+
+  def source
+    @source ||= attachments_path + 'faxes'
+  end
+
+  def destination
+    @destination ||= attachments_path + 'print_jobs'
+  end
+end
