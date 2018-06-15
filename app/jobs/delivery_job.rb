b3764029d4835f584e949cc23b9b7e56ecaf8bc9
Björn Albers <bjoernalbers@gmail.com>
2018-06-15 12:28:06 +0200

    Cleanup DeliveryJob#perform.

--- app/jobs/delivery_job.rb
+++ app/jobs/delivery_job.rb
@@ -2,8 +2,6 @@ class DeliveryJob < ActiveJob::Base
   queue_as :default
 
   def perform(document_id)
-    document = Document.find(document_id)
-    #Document::Deliverer.new(document).deliver
     Document.deliver(document_id)
   end
 end
