a95a97ee00b08c2b36fa7d45a9b915143c7ea3b9
Björn Albers <bjoernalbers@gmail.com>
2021-02-09 13:57:10 +0100

    Unicorn: Do not log to log/unicorn.log.
    The main process in a Docker container should not write to a logfile but
    to STDOUT and STDERR.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,8 +4,5 @@ timeout 30
 pid '/tmp/unicorn.pid'
 
 rails_root_dir = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-unicorn_log_file = File.join(rails_root_dir, 'log', 'unicorn.log')
 
 working_directory rails_root_dir
-stderr_path unicorn_log_file
-stdout_path unicorn_log_file
